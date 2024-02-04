8e14c26418ffa01f8f14e43d63b2276420c19c20
mk360 <36678834+mk360@users.noreply.github.com>
2024-02-04 18:01:30 +0100

    remigrate game outside of webapp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            { from: "node_modules/fire-emblem-heroes/src/assets", to: "/assets" },
+          ],
+        }),
+      );
+    }
+    return config;
+  }
 }
 
 module.exports = nextConfig
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,8 +1,5 @@
 import { useEffect } from "react";
-import Phaser from "phaser";
-import PreloadScene from "@/game/Preload";
 import { useGameContext } from "@/context/game-context";
-import MainScene from "@/game/MainScene";
 
 export default function Index() {
   const { game: world } = useGameContext();
@@ -15,29 +12,9 @@ export default function Index() {
       return;
     }
 
-    const DEFAULT_WIDTH = 750;
-    const DEFAULT_HEIGHT = 1200;
-
-    const config = {
-      type: Phaser.WEBGL,
-      backgroundColor: '#1F5E6D',
-      fps: {
-        forceSetTimeout: true,
-        target: 15,
-        min: 15,
-      },
-      scale: {
-        parent: 'phaser-game',
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: DEFAULT_WIDTH,
-        height: DEFAULT_HEIGHT
-      },
-      scene: [PreloadScene, MainScene],
-    }
-
-    var game = new Phaser.Game(config);
-    game.registry.set("world", world);
+    // @ts-ignore
+    const game = await import("fire-emblem-heroes/lib/scripts/game");
+    game.default.registry.set("world", world);
   };
 
   return null;
