07b9f4093fbb47508f9caaad0439c79d8f07b846
Muhammad Kassar <36678834+mk360@users.noreply.github.com>
2025-08-13 17:28:09 +0200

    follow configured api route

--- src/utils/fetch-moveset.ts
+++ src/utils/fetch-moveset.ts
@@ -7,7 +7,7 @@ export default async function fetchMovesets(character: string) {
     if (MOVESET_CACHE[character]) return MOVESET_CACHE[character];
     const urlParams = new URLSearchParams();
     urlParams.append("name", encodeURIComponent(character));
-    const resp = await fetch(`http://localhost:3800/moveset?${urlParams.toString()}`);
+    const resp = await fetch(`${import.meta.env.VITE_API_URL}/moveset?${urlParams.toString()}`);
     const moveset = await resp.json() as CharacterMoveset;
     const keys = Object.keys(MOVESET_CACHE);
     if (keys.length === CACHE_LIMIT) {
